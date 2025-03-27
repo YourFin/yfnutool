afd2989325390843f1621f83e8642e67347a2505
YourFin <d.junkpobox+git@gmail.com>
2025-03-26 20:42:12 -0700

    update submodule hash

--- default.nix
+++ default.nix
@@ -7,7 +7,7 @@ rustPlatform.buildRustPackage {
   version = "0.1.0";
 
   src = ./.;
-  cargoHash = "sha256-vvJnuJwmZIgoDHSR5/KQ8Iuh9FelKlqIy2PW8UjooS4=";
+  cargoHash = "sha256-Y0vI1Hm9fXBsQDKxpR/s1d05D4gP78ZF+nTmaB4Vmos";
   postInstall = ''
     mkdir -p $out/share/nushell/vendor/autoload
     cp -r $src/nu-mod/yfnutool $out/share/nushell/vendor/autoload/yfnutool
