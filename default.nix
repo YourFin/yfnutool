d6ba217aec312abc366f90e5697ef4eee10684c5
YourFin <src@patrickn.co>
2025-06-22 14:25:53 -0700

    chore: update dependencies

--- default.nix
+++ default.nix
@@ -7,7 +7,7 @@ rustPlatform.buildRustPackage {
   version = "0.1.0";
 
   src = ./.;
-  cargoHash = "sha256-Y0vI1Hm9fXBsQDKxpR/s1d05D4gP78ZF+nTmaB4Vmos";
+  cargoHash = "sha256-69g/Jau2Yz7kvJGHqGv8Nf5qCgcJhXCcup1evoLmry4=";
   postInstall = ''
     mkdir -p $out/share/nushell/vendor/autoload
     cp -r $src/nu-mod/yfnutool $out/share/nushell/vendor/autoload/yfnutool
