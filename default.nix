b8cceb521f7e99427f46b8b5309ce315c2611e64
YourFin <src@patrickn.co>
2025-08-07 16:43:12 -0700

    Move to lockfile instead of cargo hash

--- default.nix
+++ default.nix
@@ -7,7 +7,7 @@ rustPlatform.buildRustPackage {
   version = "0.1.0";
 
   src = ./.;
-  cargoHash = "sha256-69g/Jau2Yz7kvJGHqGv8Nf5qCgcJhXCcup1evoLmry4=";
+  cargoLock.lockFile = ./Cargo.lock;
   postInstall = ''
     mkdir -p $out/share/nushell/vendor/autoload
     cp -r $src/nu-mod/yfnutool $out/share/nushell/vendor/autoload/yfnutool
